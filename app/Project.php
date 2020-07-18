fb7d270eea7997365e8827b2f95ddf60625efa86
Nicole Zambrano <znicole473@gmail.com>
2020-07-18 17:40:03 -0500

    #1 #2 #3 #4 model, migration, seeder Project

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-
+    protected $fillable = ['status', 'title', 'general_objective', 'specifics_objective', 'report_pdf'];
 }
