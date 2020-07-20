dbd42620fd6f44afba30fcbf484f46ae79398603
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-07-19 20:47:19 -0500

    #6 Controladores

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Teachers;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        return Teachers::all();
+    }
+
+    public function show (Teachers $teacher)
+    {
+        return $teacher;
+    }
+
+    public function store (Request $request)
+    {
+        $teacher = Teachers::create($request->all());
+        return response()->json($teacher, 201);
+    }
+
+    public function update (Request $request, Teachers $teacher)
+    {
+        $teacher->update($request->all());
+        return response()->json($teacher, 200);
+    }
+
+    public function delete (Teachers $teacher)
+    {
+        $teacher->delete();
+        return response()->json(null, 204);
+    }
+}
