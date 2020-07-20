833de084029b7a70385f0d7053a6076f66e2b57f
Nicole Zambrano <znicole473@gmail.com>
2020-07-19 21:31:00 -0500

    #8 Autenticacion JWT

--- routes/api.php
+++ routes/api.php
@@ -20,29 +20,32 @@ use Illuminate\Http\Request;
 Route::post('register','UserController@register');
 Route::post('login', 'UserController@authenticate');
 
-//teachers plans
-Route::get('teachers-plans','TeacherPlanController@index');
-Route::get('teachers-plans','TeacherPlanController@show');
-Route::post('teachers-plans','TeacherPlanController@store');
-Route::put('teachers-plans/{teacherplan}','TeacherPlanController@update');
-Route::delete('teachers-plans/{teacherplan}','TeacherPlanController@delete');
+Route::group(['middleware' => ['jwt.verify']], function() {
+    //teachers plans
+    Route::get('teachers-plans','TeacherPlanController@index');
+    Route::get('teachers-plans','TeacherPlanController@show');
+    Route::post('teachers-plans','TeacherPlanController@store');
+    Route::put('teachers-plans/{teacherplan}','TeacherPlanController@update');
+    Route::delete('teachers-plans/{teacherplan}','TeacherPlanController@delete');
 
 //project
-Route::get('projects','ProjectController@index');
-Route::get('projects/{project}','ProjectController@show');
-Route::post('project','ProjectController@store');
-Route::put('projects/{project}','ProjectController@update');
-Route::delete('projects/{project}','ProjectController@delete');
+    Route::get('projects','ProjectController@index');
+    Route::get('projects/{project}','ProjectController@show');
+    Route::post('project','ProjectController@store');
+    Route::put('projects/{project}','ProjectController@update');
+    Route::delete('projects/{project}','ProjectController@delete');
 
 
 //student
-Route::get('students','StudentController@index');
-Route::get('students/{student}','StudentController@show');
-Route::put('students/{student}','StudentController@update');
-Route::delete('students/{student}','StudentController@delete');
+    Route::get('students','StudentController@index');
+    Route::get('students/{student}','StudentController@show');
+    Route::put('students/{student}','StudentController@update');
+    Route::delete('students/{student}','StudentController@delete');
 
 //teacher
-Route::get('teachers','TeacherController@index');
-Route::get('teachers/{teacher}','TeacherController@show');
-Route::put('teachers/{teacher}','TeacherController@update');
-Route::delete('teachers/{teacher}','TeacherController@delete');
+    Route::get('teachers','TeacherController@index');
+    Route::get('teachers/{teacher}','TeacherController@show');
+    Route::put('teachers/{teacher}','TeacherController@update');
+    Route::delete('teachers/{teacher}','TeacherController@delete');
+
+});
