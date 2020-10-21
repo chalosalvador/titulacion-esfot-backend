eada232822960f55063956780f6ff336ad62e504
ChantalMorales <chantal.morales@epn.edu.ec>
2020-10-21 16:04:44 -0500

    #38 notificacion de correcciones para el profesor

---
+++ resources/views/emails/projects/comment/teacher.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Correcciones realizadas por el estudiante.
+## El plan: {{$project->title}}.
+## Registra las correcciones de lo que solicitaste.
+## Dirigete a la plataforma para verificar.
+
+
+@endcomponent
