ac7645d73dfaa11fb161f9389d0f77d8c9a9c3e3
ChantalMorales <chantal.morales@epn.edu.ec>
2021-04-05 15:08:42 -0500

    #77 corrección notificaciones

--- config/mail.php
+++ config/mail.php
@@ -87,6 +87,10 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
+    'to' => [
+        'address' => 'chantal.morales@epn.edu.ec',
+        'name' => 'pruebas'
+    ],
 
     /*
     |--------------------------------------------------------------------------
