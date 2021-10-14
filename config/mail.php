ec58a8f5d4c44fd3b9625bdfe0b1d42500493970
Kevin <kjsegovia144@hotmail.es>
2021-10-14 15:08:30 -0500

    Correccion envio email

--- config/mail.php
+++ config/mail.php
@@ -87,10 +87,10 @@ $config = [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-    'to' => [
-        'address' => 'chantal.morales@epn.edu.ec',
-        'name' => 'pruebas'
-    ],
+//    'to' => [
+//        'address' => 'chantal.morales@epn.edu.ec',
+//        'name' => 'pruebas'
+//    ],
 
 
     /*
